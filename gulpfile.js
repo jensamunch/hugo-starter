1ac5bf3706893ec255bd47a25d3a56e5dcfbcdc2
Jens Munch <jens.a.munch@gmail.com>
2018-06-12 15:35:48 +0100

    fixed sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var cleancss = require('gulp-clean-css');
 var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
 
 var paths = {
   styles: {
@@ -24,6 +25,19 @@ var paths = {
 /*
  * Define our tasks using plain functions
  */
+
+ function stylesWatch() {
+   return gulp.src(paths.styles.src)
+     .pipe(sourcemaps.init())
+     .pipe(sass().on('error', sass.logError))
+     .pipe(sourcemaps.write())
+     .pipe(rename({
+      basename: 'main',
+      suffix: '.min'
+     }))
+     .pipe(gulp.dest(paths.styles.dest));
+ }
+
 function styles() {
   return gulp.src(paths.styles.src)
     .pipe(sass())
@@ -52,7 +66,7 @@ function scripts() {
 
 function watch() {
   gulp.watch(paths.scripts.src, scripts);
-  gulp.watch(paths.styles.src, styles);
+  gulp.watch(paths.styles.src, stylesWatch);
 }
 
 /*
